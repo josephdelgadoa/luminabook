7dd00e6a7a05c985589461fdcc03428a5f994c0a
Joseph Delgado <josephdelgadoa@gmail.com>
2025-12-30 23:50:56 -0800

    fix: polyfill crypto.randomUUID for non-secure contexts

---
+++ src/polyfills.ts
@@ -0,0 +1,16 @@
+// Polyfill for crypto.randomUUID on non-secure contexts (HTTP)
+if (typeof window !== 'undefined' && !window.crypto) {
+    // @ts-ignore
+    window.crypto = {};
+}
+
+if (typeof window !== 'undefined' && !window.crypto.randomUUID) {
+    // @ts-ignore
+    window.crypto.randomUUID = () => {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    };
+    console.log("Applied crypto.randomUUID polyfill for non-secure context.");
+}
